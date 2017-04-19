44b610f6ba438d8d2f638de14d0e95a132f3194b
madprops/mc <manuelchaves@gmail.com>
2017-04-18 23:03:15 -0600

    instrfx

--- base.js
+++ base.js
@@ -564,8 +564,9 @@ function instructions()
     s += "The prices for each profit point are shown in the table below:<br><br>";
     s += "<table cellspacing=0><tr><th>Profit</th><th>Buy Price</th><th>Sell Price</th></tr><tr><td>-1,000,000</td><td>1,000,000</td><td>0</td></tr><tr><td>-800,000</td><td>800,000</td><td>0</td></tr><tr><td>-600,000</td><td>600,000</td><td>0</td></tr><tr><td>-400,000</td><td>400,000</td><td>0</td></tr><tr><td>-200,000</td><td>200,000</td><td>0</td></tr><tr><td>0</td><td>0</td><td>0</td></tr><tr><td>200,000</td><td>200,000 x 5</td><td>200,000</td></tr><tr><td>400,000</td><td>400,000 x 5</td><td>400,000 x 2</td></tr><tr><td>600,000</td><td>600,000 x 5</td><td>600,000 x 3</td></tr><tr><td>800,000</td><td>800,000 x 5</td><td>800,000 x 4</td></tr><tr><td>1,000,000</td><td>1,000,000 x 5</td><td>1,000,000 x 5</td></tr></table><br>";
     s += "Buying low and selling high is a major source of points.<br><br>";
-    s += "Check the direction to see if the profit is going to increase or decrease.<br><br>";
     s += "Changes in profit are either +200,000 or -200,000 per tick.<br><br>";
+    s += "Check the direction to see if the profit is going to increase or decrease.<br><br>";
+    s += "The direction changes when an element reaches 1,000,000 or -1,000,000 profit.<br><br>";
     s += "You can click any part of the tile to buy or sell not just the button.<br><br>";
     s += "You can change the seed (#) to have predictable initial configurations.<br><br>";
     s += "You can change the speed of the game which changes the interval between ticks.";
