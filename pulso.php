3bf78da229b13f98d9744420e42a4d7c7e448ed7
madprops/mc <manuelchaves@gmail.com>
2017-05-02 09:42:24 -0600

    gitdude

--- pulso.php
+++ pulso.php
@@ -1,7 +1,16 @@
 <?php
 
+	$ip = getenv('HTTP_CLIENT_IP')?:
+	getenv('HTTP_X_FORWARDED_FOR')?:
+	getenv('HTTP_X_FORWARDED')?:
+	getenv('HTTP_FORWARDED_FOR')?:
+	getenv('HTTP_FORWARDED')?:
+	getenv('REMOTE_ADDR');
+
+
+
 	chdir("/var/www/elements/");
-	exec('sudo git pull  2>&1', $output);
+	exec('sudo -u gitdude git pull  2>&1', $output);
 	echo implode("<br/>\n", $output);
 
 ?>
