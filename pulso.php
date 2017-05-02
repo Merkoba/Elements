a9e1d7bfb866ab77aa43418a1b39b40333fc870b
madprops/mc <manuelchaves@gmail.com>
2017-05-02 09:17:49 -0600

    pulsotest

--- pulso.php
+++ pulso.php
@@ -1,7 +1,7 @@
 <?php
 
 	chdir("/var/www/elements/");
-	exec('git pull  2>&1', $output);
+	exec('sudo git pull  2>&1', $output);
 	echo implode("<br/>\n", $output);
 	echo "<h3 align=center>Done</h3>";
 
